b4ed754d66a23fd5afd1235100c9daa5164d38c7
panicbit <panicbit.dev@gmail.com>
2020-12-05 22:15:41 +0100

    day1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -2,5 +2,22 @@
 
 #[aoc(2020, 01, 1)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    let entries = input
+        .split_whitespace()
+        .map(|entry| entry.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    
+    for (i, entry1) in entries.iter().enumerate() {
+        for entry2 in &entries[i+1..] {
+            let sum = entry1 + entry2;
+
+            if sum == 2020 {
+                let product = entry1 * entry2;
+
+                return product
+            }
+        }
+    }
+
+    panic!("no result found")
 }
