8638e13d899afb983a2ef808f7c330974d739936
panicbit <panicbit.dev@gmail.com>
2020-12-06 15:03:14 +0100

    day04

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,6 +1,44 @@
 #[macro_use] extern crate aoc;
 
+use std::collections::BTreeMap;
+
 #[aoc(2020, 04, 1)]
 fn main(input: &str) -> i32 {
-    unimplemented!()
+    let passports = input
+        .split("\n\n")
+        .map(|passport| passport
+            .split_whitespace()
+            .map(|field| {
+                let mut fields = field.split(':');
+                (fields.next().unwrap(), fields.next().unwrap())
+            })
+            .collect::<BTreeMap<_, _>>()
+        );
+
+    let required_fields = [
+        "byr",
+        "iyr",
+        "eyr",
+        "hgt",
+        "hcl",
+        "ecl",
+        "pid",
+    ];
+
+    let mut num_valid_passports = 0;
+
+    for passport in passports {
+        let num_present_fields = required_fields
+            .iter()
+            .filter(|field| passport.contains_key(*field))
+            .count();
+        let num_missing_fields = required_fields.len() - num_present_fields;
+        let is_valid = num_missing_fields == 0;
+
+        if is_valid {
+            num_valid_passports += 1;
+        }
+    }
+
+    num_valid_passports
 }
