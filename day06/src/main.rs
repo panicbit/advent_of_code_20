80c96940ec1c27973ab4ac9d7247508c9be2c0e6
panicbit <panicbit.dev@gmail.com>
2020-12-06 20:50:09 +0100

    day06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,17 @@
 #[macro_use] extern crate aoc;
+use std::collections::BTreeSet;
 
 #[aoc(2020, 06, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    input
+    .split("\n\n")
+    .map(|group| group
+        .lines()
+        .flat_map(|answers| answers
+            .chars()
+        )
+        .collect::<BTreeSet<_>>()
+        .len()
+    )
+    .sum()
 }
